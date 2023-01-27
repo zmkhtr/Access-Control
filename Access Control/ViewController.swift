88e35a9eb5e8b89b35e6f947d17ab6486c0d1828
zmkhtr <azammukhtar3@gmail.com>
2023-01-27 17:05:52 +0700

    Initial Commit

---
+++ Access Control/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Access Control
+//
+//  Created by Azam Mukhtar on 27/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
